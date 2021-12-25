42a4169ae836d36cda14d5b561c1125a0e297470
seunghwan-won <tmdghks24@naver.com>
2021-12-25 20:47:40 +0900

    jdk 동적 프록시는 인터페이스가 필수

--- src/main/java/hello/AdvencedApplication.java
+++ src/main/java/hello/AdvencedApplication.java
@@ -2,12 +2,15 @@ package hello;
 
 import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v2_dynamic_proxy.DynamicFilterProxyConfig;
+import hello.proxy.config.v2_dynamic_proxy.DynamicProxyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 //@Import({AppV1Config.class, AppV2Config.class})
-@Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class})
+//@Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class})
+@Import({DynamicFilterProxyConfig.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class AdvencedApplication {
 
---
+++ src/main/java/hello/proxy/config/v2_dynamic_proxy/handlerr/LogTraceBasicHandler.java
@@ -0,0 +1,33 @@
+package hello.proxy.config.v2_dynamic_proxy.handlerr;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class LogTraceBasicHandler implements InvocationHandler {
+    private final LogTrace logTrace;
+    private final Object target;
+
+    public LogTraceBasicHandler(LogTrace logTrace, Object target) {
+        this.logTrace = logTrace;
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        TraceStatus status = null;
+        try {
+            String message = method.getDeclaringClass().getSimpleName() + "."
+                    + method.getName() + "()";
+            status = logTrace.begin(message);
+            Object result = method.invoke(target, args);
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
