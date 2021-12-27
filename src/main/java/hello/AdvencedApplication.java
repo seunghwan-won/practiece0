eadeef98424e765a8a772a507b7146bf59896ed5
seunghwan-won <tmdghks24@naver.com>
2021-12-28 01:01:29 +0900

    빈후처리기 : 컴포넌트 스캔의 대상도 프록시 적용이 가능함

--- src/main/java/hello/AdvencedApplication.java
+++ src/main/java/hello/AdvencedApplication.java
@@ -6,6 +6,7 @@ import hello.proxy.config.v2_dynamic_proxy.DynamicFilterProxyConfig;
 import hello.proxy.config.v2_dynamic_proxy.DynamicProxyConfig;
 import hello.proxy.config.v3_prox_factory.ProxyFactoryConfigV1;
 import hello.proxy.config.v3_prox_factory.ProxyFactoryConfigV2;
+import hello.proxy.config.v4_postProcessor.PostProcessorConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -13,7 +14,9 @@ import org.springframework.context.annotation.Import;
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class})
 //@Import({DynamicFilterProxyConfig.class})
-@Import({ProxyFactoryConfigV2.class})
+//@Import({ProxyFactoryConfigV2.class})
+
+@Import({PostProcessorConfig.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class AdvencedApplication {
 
