8b517eabd827d7ab70a931638e14563da92c16be
unknown <20210476@I-BCCARD.BIZ>
2021-12-27 18:21:33 +0900

    구체클래스만 있는 경우

--- src/main/java/hello/AdvencedApplication.java
+++ src/main/java/hello/AdvencedApplication.java
@@ -5,6 +5,7 @@ import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.config.v2_dynamic_proxy.DynamicFilterProxyConfig;
 import hello.proxy.config.v2_dynamic_proxy.DynamicProxyConfig;
 import hello.proxy.config.v3_prox_factory.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_prox_factory.ProxyFactoryConfigV2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.Import;
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class})
 //@Import({DynamicFilterProxyConfig.class})
-@Import({ProxyFactoryConfigV1.class})
+@Import({ProxyFactoryConfigV2.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class AdvencedApplication {
 
