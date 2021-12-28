0c2daf63c62afe375f949c7dd0242dea439d27b9
unknown <20210476@I-BCCARD.BIZ>
2021-12-28 13:15:40 +0900

    @AspectJ

--- src/main/java/hello/AdvencedApplication.java
+++ src/main/java/hello/AdvencedApplication.java
@@ -1,6 +1,7 @@
 package hello;
 
 import hello.proxy.config.v5_auto_proxy.AutoProxyConfig;
+import hello.proxy.config.v6_aspect.AspectConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -10,7 +11,8 @@ import org.springframework.context.annotation.Import;
 //@Import({DynamicFilterProxyConfig.class})
 //@Import({ProxyFactoryConfigV2.class})
 //@Import({PostProcessorConfig.class})
-@Import({AutoProxyConfig.class})
+//@Import({AutoProxyConfig.class})
+@Import({AspectConfig.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class AdvencedApplication {
 
