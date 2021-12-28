aae1a5dba09a65c8ba53258899a57b565751a496
unknown <20210476@I-BCCARD.BIZ>
2021-12-28 14:38:39 +0900

    프로젝트 생성

--- src/main/java/hello/AdvencedApplication.java
+++ src/main/java/hello/AdvencedApplication.java
@@ -1,19 +1,9 @@
 package hello;
 
-import hello.proxy.config.v5_auto_proxy.AutoProxyConfig;
-import hello.proxy.config.v6_aspect.AspectConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
-//@Import({AppV1Config.class, AppV2Config.class})
-//@Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class})
-//@Import({DynamicFilterProxyConfig.class})
-//@Import({ProxyFactoryConfigV2.class})
-//@Import({PostProcessorConfig.class})
-//@Import({AutoProxyConfig.class})
-@Import({AspectConfig.class})
-@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+@SpringBootApplication
 public class AdvencedApplication {
 
 	public static void main(String[] args) {
--- src/main/java/hello/advenced/app/trace/LogTrace.java
+++
@@ -1,7 +0,0 @@
-package hello.advenced.app.trace;
-
-public interface LogTrace {
-     TraceStatus begin(String message);
-     void end(TraceStatus traceStatus);
-     void exception(TraceStatus traceStatus, Exception e);
-}
