6a2ca59bcb64048bc6b72f94ca44b77f025ca70f
unknown <20210476@I-BCCARD.BIZ>
2021-12-28 18:11:57 +0900

    스프링 aop

---
+++ src/main/java/hello/aop/order/aop/AspectV5Transaction.java
@@ -0,0 +1,49 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.core.annotation.Order;
+
+@Slf4j
+@Aspect
+public class AspectV5Transaction {
+    // hello.aop.order. 패키지와 하위 패키지 이면서 클래서 이름 패턴이 *Service
+//    @Around("Pointcuts.allOrderAndService()")
+//    public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
+//        try {
+//            log.info("[트랜잭션 시작] {}", joinPoint.getSignature());
+//            Object result = joinPoint.proceed();
+//            log.info("[트랜잭션 커밋] {}", joinPoint.getSignature());
+//            return result;
+//        } catch (Exception e) {
+//            log.info("[트랜잭션 롤백] {}", joinPoint.getSignature());
+//            throw e;
+//        } finally {
+//            log.info("[리소스 릴리즈] {}", joinPoint.getSignature());
+//        }
+//    }
+
+    @Before("Pointcuts.allOrderAndService()")
+    public void doBefore(JoinPoint joinPoint) {
+        log.info("[before] {}", joinPoint.getSignature());
+    }
+
+    @AfterReturning(value = "Pointcuts.allOrderAndService()", returning = "result")
+    public void doAfterReturning(JoinPoint joinPoint, Object result) {
+        log.info("[afterReturning] {} result={}", joinPoint.getSignature(), result);
+    }
+
+
+    @AfterThrowing(value = "Pointcuts.allOrderAndService()", throwing = "ex")
+    public void doAfterThrowing(JoinPoint joinPoint, Exception ex) {
+        log.info("[afterThrowing] {} ex={}", joinPoint.getSignature(), ex);
+    }
+
+
+    @After("Pointcuts.allOrderAndService()")
+    public void doAfter(JoinPoint joinPoint) {
+        log.info("[after] {}", joinPoint.getSignature());
+    }
+}
